0c4b6e54a44fa25053ef0f78729e7ac56f9046b8
John Eckert <johnteckert@gmail.com>
2019-03-24 15:22:01 -0400

    added env var port for heroku

--- xptracker.js
+++ xptracker.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser");
 const app = express()
-const port = 8081;
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
+const port = process.env.PORT || app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
 
 let xp = 0;
