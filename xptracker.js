417bbc87025fba7d745c5898cdd469aa956f80f6
John Eckert <johnteckert@gmail.com>
2019-06-15 22:02:59 -0400

    debuggin heroku

--- xptracker.js
+++ xptracker.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express()
 const port = process.env.PORT || app.use(bodyParser.urlencoded({ extended: false }));
 //const port = 9091
-const config = require('./config')
+const config = require('./config') || {}
 const apiKey = process.env.AIRTABLE || config.airtable;
 
 var Airtable = require('airtable');
